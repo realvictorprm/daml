f221243503bc9b16f5e61dbb1395e83e073839c8
Nick Chapman <nick.chapman@digitalasset.com>
2021-06-07 10:46:01 +0100

    bench-script

---
+++ bench.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+wi=2
+i=10
+
+whatBranch() {
+    git rev-parse --abbrev-ref HEAD
+}
+
+bench() {
+    git checkout $1
+    bazel run //daml-lf/scenario-interpreter:scenario-perf -- -f 1 -i $i -wi $wi
+}
+
+run() {
+    bench $1 2>&1 | grep 'Average]' | xargs echo $1
+}
+
+here=$(whatBranch)
+#base=main
+base=dc79830239
+
+
+run $here
+run $here
+
+run $base
+run $base
+
+run $here
+run $here
+
+run $base
+run $base
+
+#git checkout $here
